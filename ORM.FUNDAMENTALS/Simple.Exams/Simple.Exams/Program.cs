5380756fd3f3277f105f2a52a53498ca0480e1ab
Angel Petrov <64444672+Petrozza@users.noreply.github.com>
2021-02-20 01:49:03 +0200

    add

---
+++ ORM.FUNDAMENTALS/Simple.Exams/Simple.Exams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Simple.Exams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var town = new Town
+            {
+                Name = "Pernik"
+
+            };
+            
+        }
+    }
+}
---
+++ ORM.FUNDAMENTALS/Simple.Exams/Simple.Exams/VRank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Simple.Exams
+{
+    public partial class VRank
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public long? Rank { get; set; }
+    }
+}
